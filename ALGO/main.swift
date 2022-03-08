25095f14dcffee9dcf4bfc22974d292e0f534f54
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-03-07 21:03:24 -0800

    solved 2 more algos

--- ALGO/main.swift
+++ ALGO/main.swift
@@ -26,6 +26,8 @@ print(isUnique("abc"))
 print(isUnique("abcdefhjkloiuy"))
 print(isUnique("abcccccc"))
 print(isUnique("abcdeghijklmnopp"))
+print("")
+
 
 
 //MARK: Question 2: Permutations
@@ -42,10 +44,13 @@ func isPermutated(_ text: String, _ perm: String) -> Bool {
 print(isPermutated("abd", "dab"))
 print(isPermutated("doggy", "godgy"))
 print(isPermutated("kiwi", "apple"))
+print("")
 
 
 //MARK: Question 3: URLify
 //-Replace all blank space with ASCII symbol %20
+
+//MY  SOLUTION
 
 func urlify(_ url: String, length: Int) -> String {
     let originalURLString = url
@@ -60,12 +65,90 @@ func urlify(_ url: String, length: Int) -> String {
     }
     
     for char in finalURLString {
+        if finalURLArray.count < length {
         finalURLArray.append(char)
+        }
     }
     
     return String(finalURLArray)
 }
 
-print(urlify("My Home Page    ", length: 50))
-print(urlify("My Home Page    ", length: 75))
-print(urlify("My Home Page    ", length: 100))
+print(urlify("My Home Page   ", length: 100))
+print(String(repeating: "a", count: 100))
+print(urlify("My Home Page            ", length: 75))
+print(String(repeating: "a", count: 75))
+print(urlify("My Home Page     ", length: 50))
+print(String(repeating: "a", count: 50))
+print("")
+
+
+//Course Solution (doesn't work?)
+
+func urlify2(_ url: String, length: Int) -> String {
+    var result = Array<Character>(repeating: " ", count: length)
+    
+    let url = url.trimmingCharacters(in: .whitespacesAndNewlines)
+    
+    let urlChars = Array(url)
+    
+    var pointer = 0
+    
+    for i in 0..<urlChars.count {
+        if urlChars[i] != " " {
+            result[pointer] = urlChars[i]
+            pointer += 1
+        } else {
+            result[pointer] = "%"
+            result[pointer] = "2"
+            result[pointer] = "0"
+            pointer = pointer + 3
+        }
+    }
+    
+    return String(result)
+}
+
+//print(urlify2("My Home Page   ", length: 100))
+//print(String(repeating: "a", count: 100))
+//print(urlify2("My Home Page            ", length: 75))
+//print(String(repeating: "a", count: 75))
+//print(urlify2("My Home Page     ", length: 50))
+//print(String(repeating: "a", count: 50))
+
+
+
+//MARK: Question 4: One away
+//Given 2 strings, write a function to see if they are more than one or 0 edits away
+//edits include inserting, removing, or replacing a character
+
+func oneAway(_ first: String, _ second: String) -> Bool {
+    var i = 0
+    var j = 0
+    
+    let firstStringChars = Array(first)
+    let secondStringChars = Array(second)
+    
+    if first.count - second.count > 1 || second.count - first.count > 1 {
+        return false
+    } else {
+        while i < first.count && j < second.count {
+            if firstStringChars[i] != secondStringChars[j] {
+                if i != j {
+                    return false
+                }
+                j += 1
+            } else {
+                i += 1
+                j += 1
+            }
+        }
+    }
+    
+    return true
+}
+
+print(oneAway("ben", "bean"))
+print(oneAway("ben", "beanie baby"))
+print(oneAway("ben", "be"))
+print(oneAway("ben", "ben"))
+print(oneAway("ben", "bonjour"))
